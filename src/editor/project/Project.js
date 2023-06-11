96ff4d8eb1aec01aed98f6e6d19f5c3bb64574e0
ufrs12 <ufrs12@yandex.ru>
2023-06-11 15:37:31 +0300

    Removed default nodes
    _

--- src/editor/project/Project.js
+++ src/editor/project/Project.js
@@ -26,21 +26,21 @@
     this.history = new b3e.project.HistoryManager(this._editor, this);
 
     this.nodes.add(b3e.Root, true);
-    this.nodes.add(b3.Sequence, true);
-    this.nodes.add(b3.Priority, true);
-    this.nodes.add(b3.MemSequence, true);
-    this.nodes.add(b3.MemPriority, true);
-    this.nodes.add(b3.Repeater, true);
-    this.nodes.add(b3.RepeatUntilFailure, true);
-    this.nodes.add(b3.RepeatUntilSuccess, true);
-    this.nodes.add(b3.MaxTime, true);
-    this.nodes.add(b3.Inverter, true);
-    this.nodes.add(b3.Limiter, true);
-    this.nodes.add(b3.Failer, true);
-    this.nodes.add(b3.Succeeder, true);
-    this.nodes.add(b3.Runner, true);
-    this.nodes.add(b3.Error, true);
-    this.nodes.add(b3.Wait, true);
+   // this.nodes.add(b3.Sequence, true);
+   // this.nodes.add(b3.Priority, true);
+   // this.nodes.add(b3.MemSequence, true);
+   // this.nodes.add(b3.MemPriority, true);
+   // this.nodes.add(b3.Repeater, true);
+   // this.nodes.add(b3.RepeatUntilFailure, true);
+   // this.nodes.add(b3.RepeatUntilSuccess, true);
+   // this.nodes.add(b3.MaxTime, true);
+   // this.nodes.add(b3.Inverter, true);
+   // this.nodes.add(b3.Limiter, true);
+   // this.nodes.add(b3.Failer, true);
+   // this.nodes.add(b3.Succeeder, true);
+   // this.nodes.add(b3.Runner, true);
+   // this.nodes.add(b3.Error, true);
+   // this.nodes.add(b3.Wait, true);
 
     this._applySettings(this._editor._settings);
     this.history.clear();
